1b2fedc0b6e2c9bb4162065e15983d77bcbc2819
atanad nedyalkov <atanas.nedyalkov92@gmail.com>
2022-10-09 13:46:05 +0300

    Add project files.

---
+++ 2.Get Persons.js
@@ -0,0 +1,18 @@
+class Person {
+    constructor(fName,Lname,age,email){
+    this.firstName = fName;
+    this.lastName = Lname;
+    this.age = age;
+    this.email = email;
+    }
+}
+return [
+
+    new Person('Anna', 'Simpson', 22, 'anna@yahoo.com'),
+    new Person('SoftUni'),
+    new Person('Stephan','Johnson', 25),
+    new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com')  
+]
+
+
+
